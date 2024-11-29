d5f68a5ca7813e3245943a0ac5ec7c13f0a8a386
Rajesh Singh <rajeshnksingh@gmail.com>
2024-11-29 15:40:29 +0530

    Recurrent Neural Network Shakespeare

---
+++ utils/__inti__.py
@@ -0,0 +1,34 @@
+Deep Learning Overview
+├── Introduction to Deep Neural Networks
+│   ├── Recurrent Neural Networks (RNN)
+│   ├── Convolutional Neural Networks (CNN)
+│   ├── Long Short-Term Memory Networks (LSTM)
+│   ├── Deep Belief Networks (DBN)
+│   ├── Semantic Hashing
+├── Building and Training Deep Learning Models
+│   ├── Building Basic Neural Networks
+│   │   ├── Using Keras with TensorFlow
+│   │   ├── Building Models in PyTorch
+│   │   └── TensorFlow 2.x
+│   ├── Training Deep Neural Networks
+│   ├── Troubleshooting Deep Learning Models
+│   ├── Transfer Learning
+│   │   ├── Inductive Learning
+│   │   ├── Unsupervised Learning
+│   │   └── Transductive Learning
+│   ├── Tuning Deep Learning Models
+│   └── Application of Multi-Processing in Deep Learning
+├── Tools and Techniques for Deep Learning
+│   ├── TensorFlow 2.x
+│   ├── PyTorch
+│   ├── Other Tools and Libraries
+├── Deep Learning Trends
+│   ├── Advances in Architectures
+│   ├── Emerging Techniques
+│   └── Future Directions
+├── Deep Learning Applications
+│   ├── Case Studies
+│   └── Building Deep Learning Projects
+│       └── Log Model Example
+└── Special Topics
+    └── Semantic Hashing
